6d10e12402c58101b1684d249551eb800ad777f9
nicklhy <nicklhy@gmail.com>
2017-01-16 16:57:22 +0800

    first commit

---
+++ barycentric_interpolate_model_cifar10.py
@@ -0,0 +1,152 @@
+import os
+import argparse
+import cPickle
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from common import find_mxnet, data
+from common.util import download_file
+import mxnet as mx
+import numpy as np
+
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+def download_cifar10():
+    data_dir="data"
+    fnames = (os.path.join(data_dir, "cifar10_train.rec"),
+              os.path.join(data_dir, "cifar10_val.rec"))
+    download_file('http://data.mxnet.io/data/cifar10/cifar10_val.rec', fnames[1])
+    download_file('http://data.mxnet.io/data/cifar10/cifar10_train.rec', fnames[0])
+    return fnames
+
+def load_params(filename):
+    params = {
+        'arg_params': {},
+        'aux_params': {},
+    }
+
+    for k,v in mx.nd.load(filename).items():
+        tp, name = k.split(':', 1)
+        if tp == 'arg':
+            params['arg_params'][name] = v
+        elif tp == 'aux':
+            params['aux_params'][name] = v
+
+    return params
+
+
+if __name__ == '__main__':
+    # download data
+    (train_fname, val_fname) = download_cifar10()
+
+    parser = argparse.ArgumentParser(description="interpolate score",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--net-json', type=str, required=True,
+                        help='symbol\'s json file')
+    parser.add_argument('--params1', type=str, required=True,
+                        help='the first network weights file')
+    parser.add_argument('--params2', type=str, required=True,
+                        help='the second network weights file')
+    parser.add_argument('--params3', type=str, required=True,
+                        help='the second network weights file')
+    parser.add_argument('--batch-size', type=int, default=16,
+                       help='the batch size')
+    parser.add_argument('--alpha1', type=float, default=0.0,
+                       help='lower bound of alpha')
+    parser.add_argument('--alpha2', type=float, default=1.0,
+                       help='higher bound of alpha')
+    parser.add_argument('--alpha-num', type=int, default=20,
+                       help='number of alpha values')
+    parser.add_argument('--beta-num', type=int, default=20,
+                       help='number of beta values')
+    parser.add_argument('--gpus', type=str,
+                        help='list of gpus to run, e.g. 0 or 0,2,5. empty means using cpu')
+    parser.add_argument('--output', type=str,
+                        help='output file')
+    parser.add_argument('--kv-store', type=str, default='device',
+                        help='key-value store type')
+    data.add_data_args(parser)
+    data.add_data_aug_args(parser)
+    data.set_data_aug_level(parser, 2)
+    parser.set_defaults(
+        # data
+        data_train     = train_fname,
+        data_val       = val_fname,
+        num_classes    = 10,
+        num_examples  = 50000,
+        image_shape    = '3,28,28',
+        pad_size       = 4,
+        # train
+        batch_size     = 128,
+    )
+    args = parser.parse_args()
+
+    params1 = load_params(args.params1)
+    params2 = load_params(args.params2)
+    params3 = load_params(args.params3)
+
+    kv = mx.kvstore.create(args.kv_store)
+    train, val = data.get_rec_iter(args, kv)
+
+    # devices for training
+    devs = mx.cpu() if args.gpus is None or args.gpus is '' else [
+        mx.gpu(int(i)) for i in args.gpus.split(',')]
+
+    # create model
+    model = mx.mod.Module(
+        context       = devs,
+        symbol        = mx.sym.load(args.net_json)
+    )
+    model.bind(data_shapes=train.provide_data, label_shapes=train.provide_label)
+
+    # evaluation metrices
+    eval_metrics = mx.metric.create('accuracy')
+
+    alpha_list = np.linspace(args.alpha1, args.alpha2, args.alpha_num+1)
+    beta_list = np.linspace(args.alpha1, args.alpha2, args.beta_num+1)
+    alpha_grid, beta_grid = np.meshgrid(alpha_list, beta_list)
+    train_error_grid = np.zeros(alpha_grid.shape)
+    val_error_grid = np.zeros(alpha_grid.shape)
+    for i in range(alpha_grid.shape[0]):
+        for j in range(alpha_grid.shape[1]):
+            alpha = alpha_grid[i, j]
+            beta = beta_grid[i, j]
+            #  compute tmp weights
+            tmp_params = {
+                'arg_params': {},
+                'aux_params': {},
+            }
+            for name in tmp_params.keys():
+                for k, theta_0 in params1[name].items():
+                    theta_1 = params2[name][k]
+                    theta_2 = params3[name][k]
+                    tmp_params[name][k] = beta*(alpha*(theta_1-theta_0)+theta_0)+(1-beta)*(alpha*(theta_2-theta_0)+theta_0)
+
+            #  set tmp weights
+            model.set_params(arg_params=tmp_params['arg_params'],
+                             aux_params=tmp_params['aux_params'])
+
+            print 'alpha = %f, beta = %f ...' % (alpha, beta)
+
+            if train is not None:
+                model.score(train, eval_metrics, reset=True)
+                train_error_grid[i, j] = 1-eval_metrics.get()[1]
+                print '\ttrain error = %f' % train_error_grid[i, j]
+            if val is not None:
+                model.score(val, eval_metrics, reset=True)
+                val_error_grid[i, j] = 1-eval_metrics.get()[1]
+                print '\tval error = %f' % val_error_grid[i, j]
+
+    if args.output is None:
+        args.output = os.path.join(ROOT_DIR,
+                                   'cache',
+                                   os.path.split(args.params1)[-1].replace('.params', '')+'_'+os.path.split(args.params2)[-1].replace('.params', '')+'_'+os.path.split(args.params3)[-1].replace('.params', '')+'_barycentric_interpolate.pkl')
+        if not os.path.exists(os.path.dirname(args.output)):
+            os.mkdir(os.path.dirname(args.output))
+    with open(args.output, 'wb') as fd:
+        cPickle.dump({
+            'alpha_grid': alpha_grid,
+            'beta_grid': beta_grid,
+            'train_error_grid': train_error_grid,
+            'val_error_grid': val_error_grid,
+        }, fd)
+        print 'save log to %s' % (args.output)
---
+++ common/fit.py
@@ -0,0 +1,222 @@
+import mxnet as mx
+import logging
+import os
+import time
+
+def _get_lr_scheduler(args, kv):
+    if 'lr_factor' not in args or args.lr_factor >= 1:
+        return (args.lr, None)
+    epoch_size = args.num_examples / args.batch_size
+    if 'dist' in args.kv_store:
+        epoch_size /= kv.num_workers
+    begin_epoch = args.begin_epoch
+    step_epochs = [int(l) for l in args.lr_step_epochs.split(',')]
+    lr = args.lr
+    for s in step_epochs:
+        if begin_epoch >= s:
+            lr *= args.lr_factor
+    if lr != args.lr:
+        logging.info('Adjust learning rate to %e for epoch %d' %(lr, begin_epoch))
+
+    steps = [epoch_size * (x-begin_epoch) for x in step_epochs if x-begin_epoch > 0]
+    return (lr, mx.lr_scheduler.MultiFactorScheduler(step=steps, factor=args.lr_factor))
+
+def _load_model(args, rank=0):
+    if 'params' not in args or args.params is None:
+        return (None, None, None)
+    if args.net_json is not None:
+        sym = mx.sym.load(args.net_json)
+    else:
+        sym = None
+    arg_params = {}
+    aux_params = {}
+    for k,v in mx.nd.load(args.params).items():
+        tp, name = k.split(':', 1)
+        if tp == 'arg':
+            arg_params[name] = v
+        elif tp == 'aux':
+            aux_params[name] = v
+
+    logging.info('Loaded model %s', args.params)
+    return (sym, arg_params, aux_params)
+
+def _save_model(mod, args, rank=0):
+    if args.model_prefix is None:
+        return None
+    dst_dir = os.path.dirname(args.model_prefix)
+    if not os.path.isdir(dst_dir):
+        os.mkdir(dst_dir)
+    return mx.callback.module_checkpoint(mod, args.model_prefix if rank == 0 else "%s-%d" % (
+        args.model_prefix, rank), period=args.model_period)
+
+def add_fit_args(parser):
+    """
+    parser : argparse.ArgumentParser
+    return a parser added with args required by fit
+    """
+    train = parser.add_argument_group('Training', 'model training')
+    train.add_argument('--network', type=str,
+                       help='the neural network to use')
+    train.add_argument('--net-json', type=str,
+                       help='network json file')
+    train.add_argument('--params', type=str,
+                       help='pretrained weights')
+    train.add_argument('--begin-epoch', type=int, default=0,
+                       help='begin epoch')
+    train.add_argument('--num-layers', type=int,
+                       help='number of layers in the neural network, required by some networks such as resnet')
+    train.add_argument('--gpus', type=str,
+                       help='list of gpus to run, e.g. 0 or 0,2,5. empty means using cpu')
+    train.add_argument('--kv-store', type=str, default='device',
+                       help='key-value store type')
+    train.add_argument('--num-epochs', type=int, default=100,
+                       help='max num of epochs')
+    train.add_argument('--lr', type=float, default=0.1,
+                       help='initial learning rate')
+    train.add_argument('--lr-factor', type=float, default=0.1,
+                       help='the ratio to reduce lr on each step')
+    train.add_argument('--lr-step-epochs', type=str,
+                       help='the epochs to reduce the lr, e.g. 30,60')
+    train.add_argument('--optimizer', type=str, default='sgd',
+                       help='the optimizer type')
+    train.add_argument('--mom', type=float, default=0.9,
+                       help='momentum for sgd and nag')
+    train.add_argument('--rho', type=float, default=0.9,
+                       help='rho for AdaDelta')
+    train.add_argument('--beta1', type=float, default=0.9,
+                       help='beta1 for Adam')
+    train.add_argument('--beta2', type=float, default=0.999,
+                       help='beta2 for Adam')
+    train.add_argument('--gamma1', type=float, default=0.95,
+                       help='gamma1 for RMSProp')
+    train.add_argument('--gamma2', type=float, default=0.9,
+                       help='gamma2 for RMSProp')
+    train.add_argument('--wd', type=float, default=0.0001,
+                       help='weight decay for sgd')
+    train.add_argument('--batch-size', type=int, default=128,
+                       help='the batch size')
+    train.add_argument('--disp-batches', type=int, default=20,
+                       help='show progress for every n batches')
+    train.add_argument('--model-prefix', type=str,
+                       help='model prefix')
+    train.add_argument('--model-period', type=int, default=1,
+                       help='model snapshot period')
+    parser.add_argument('--monitor', dest='monitor', type=int, default=0,
+                        help='log network parameters every N iters if larger than 0')
+    train.add_argument('--top-k', type=int, default=0,
+                       help='report the top-k accuracy. 0 means no report.')
+    train.add_argument('--test-io', type=int, default=0,
+                       help='1 means test reading speed without training')
+    return train
+
+def fit(args, network, data_loader, **kwargs):
+    """
+    train a model
+    args : argparse returns
+    network : the symbol definition of the nerual network
+    data_loader : function that returns the train and val data iterators
+    """
+    # kvstore
+    kv = mx.kvstore.create(args.kv_store)
+
+    # logging
+    head = '%(asctime)-15s Node[' + str(kv.rank) + '] %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=head)
+    logging.info('start with arguments %s', args)
+
+    # data iterators
+    (train, val) = data_loader(args, kv)
+    if args.test_io:
+        tic = time.time()
+        for i, batch in enumerate(train):
+            for j in batch.data:
+                j.wait_to_read()
+            if (i+1) % args.disp_batches == 0:
+                logging.info('Batch [%d]\tSpeed: %.2f samples/sec' % (
+                    i, args.disp_batches*args.batch_size/(time.time()-tic)))
+                tic = time.time()
+
+        return
+
+
+    # load model
+    if 'arg_params' in kwargs and 'aux_params' in kwargs:
+        arg_params = kwargs['arg_params']
+        aux_params = kwargs['aux_params']
+    else:
+        sym, arg_params, aux_params = _load_model(args, kv.rank)
+        if sym is not None:
+            assert sym.tojson() == network.tojson()
+
+    # devices for training
+    devs = mx.cpu() if args.gpus is None or args.gpus is '' else [
+        mx.gpu(int(i)) for i in args.gpus.split(',')]
+
+    # learning rate
+    lr, lr_scheduler = _get_lr_scheduler(args, kv)
+
+    # create model
+    model = mx.mod.Module(
+        context       = devs,
+        symbol        = network
+    )
+
+    lr_scheduler  = lr_scheduler
+    optimizer_params = {
+            'learning_rate': lr,
+            'wd' : args.wd,
+            'lr_scheduler': lr_scheduler}
+    if args.optimizer.lower() == 'sgd' or args.optimizer.lower() == 'nag':
+        optimizer_params['momentum'] = args.mom
+    elif args.optimizer.lower() == 'adam':
+        optimizer_params['beta1'] = args.beta1
+        optimizer_params['beta2'] = args.beta2
+    elif args.optimizer.lower() == 'rmsprop':
+        optimizer_params['gamma1'] = args.gamma1
+        optimizer_params['gamma2'] = args.gamma2
+    if args.optimizer.lower() == 'adadelta':
+        optimizer_params['rho'] = args.rho
+
+    monitor = mx.mon.Monitor(args.monitor, pattern=".*") if args.monitor > 0 else None
+
+    # save model
+    checkpoint = _save_model(model, args, kv.rank)
+
+    initializer   = mx.init.Xavier(
+       rnd_type='gaussian', factor_type="in", magnitude=2)
+    # initializer   = mx.init.Xavier(factor_type="in", magnitude=2.34),
+    model.bind(data_shapes=train.provide_data, label_shapes=train.provide_label)
+    model.init_params(initializer=initializer,
+                      arg_params=arg_params,
+                      aux_params=aux_params)
+    #  save the initialized weights
+    arg_params, aux_params = model.get_params()
+    checkpoint(-1, model.symbol, arg_params, aux_params)
+
+    # evaluation metrices
+    eval_metrics = ['accuracy']
+    if args.top_k > 0:
+        eval_metrics.append(mx.metric.create('top_k_accuracy', top_k=args.top_k))
+
+    # callbacks that run after each batch
+    batch_end_callbacks = [mx.callback.Speedometer(args.batch_size, args.disp_batches)]
+    if 'batch_end_callback' in kwargs:
+        cbs = kwargs['batch_end_callback']
+        batch_end_callbacks += cbs if isinstance(cbs, list) else [cbs]
+
+    # run
+    model.fit(train,
+        begin_epoch        = args.begin_epoch,
+        num_epoch          = args.num_epochs,
+        eval_data          = val,
+        eval_metric        = eval_metrics,
+        kvstore            = kv,
+        optimizer          = args.optimizer,
+        optimizer_params   = optimizer_params,
+        #  initializer        = initializer,
+        arg_params         = arg_params,
+        aux_params         = aux_params,
+        batch_end_callback = batch_end_callbacks,
+        epoch_end_callback = checkpoint,
+        allow_missing      = True,
+        monitor            = monitor)
